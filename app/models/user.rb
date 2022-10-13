4f29e238b05987e4bac21f7193f75c15ba3becd3
Eugene <eugeneayonga@gmail.com>
2022-10-13 15:55:48 +0300

    Dried out user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
+
     has_many :runs 
     has_secure_password
     validates :username, presence: true, uniqueness: true
      
     def self.most_runs 
-        # self.preload(:runs).max_by do |user|
-        #     user.runs.size
-
-        # end
+       
         self.all.max_by do |user| 
             user.runs.size 
         end
@@ -15,9 +13,11 @@ class User < ApplicationRecord
             user.runs.size
         end
         user_runs.last
-    end 
+    end
+
     def favoriteRuns 
         runs = user.runs.where(favorite: true)
         
-    end 
+    end
+     
 end
