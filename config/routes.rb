6394a5e885d59052696ee9af2c941ad7674b3c92
Eugene <eugeneayonga@gmail.com>
2022-10-13 15:39:26 +0300

    Cleared out unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,22 @@ Rails.application.routes.draw do
     resources :users
 
     get '/fastest-mile', to: 'runs#fastest_mile'
-    # get '/most-runs', to: 'users#most_runs'
-    # get '/most-runs-1', to: 'users#most_runs_1'
+
     get '/longest-run', to: 'runs#longest_run'
+
     get '/find-heartrate', to: 'runs#find_heartrate'
+
     get '/me', to: 'users#show'
+
     post '/signup', to: 'users#create'
+
     post '/login', to: 'sessions#create'
+
     delete '/logout', to: 'sessions#destroy'
 
   end
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
